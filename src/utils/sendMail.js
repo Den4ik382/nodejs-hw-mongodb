37861ef5ae3648666de7315e41e085ca8e6d0191
Den4ik382 <denlikhman1@gmail.com>
2024-10-01 01:36:56 +0200

    5

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,8 +4,8 @@ import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
 const transporter = nodemailer.createTransport({
-  host: env(SMTP.SMTP_HOST),
-  port: Number(env(SMTP.SMTP_PORT)),
+  host: 'smtp-relay.brevo.com',
+  port: 587,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
